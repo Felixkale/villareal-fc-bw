6ad520a2093f665286f21389da526989c7c4227f
Felixkale <95532883+Felixkale@users.noreply.github.com>
2026-05-20 00:34:19 +0200

    Update main.jsx

--- villareal-fc-bw-FIXED/src/main.jsx
+++ villareal-fc-bw-FIXED/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Admin from './Admin'
+
+// Route /admin to Admin panel, everything else to App
+const isAdmin = window.location.pathname.startsWith('/admin')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </React.StrictMode>
 )
